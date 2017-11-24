eea4b0d83b40609712926b88073970d63263d162
Luwangel <adrien.amoros@gmail.com>
2017-11-24 12:34:38 +0100

    Use a spy instead of a global mock in random

--- __tests__/src/core/shuffler.spec.js
+++ __tests__/src/core/shuffler.spec.js
@@ -1,15 +1,9 @@
 import * as Shuffler from '../../../src/core/shuffler';
 
-const mockMathRandom = val => {
-    const mockMath = Object.create(global.Math);
-    mockMath.random = () => val;
-    global.Math = mockMath;
-};
-
 describe('Game', () => {
     describe('chooseCoords', () => {
         test('should choice random coords from a list', () => {
-            mockMathRandom(0.25);
+            jest.spyOn(global.Math, 'random').mockImplementation(() => 0.25);
 
             const coordsList = [
                 { y: 1, x: 1 },
