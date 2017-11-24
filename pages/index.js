597729743b8e66ea39a85221f2ef5270e0dcaf45
Luwangel <adrien.amoros@gmail.com>
2017-11-24 11:14:49 +0100

    Create and include the head balise

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import CustomHead from '../src/components/head';
 
 export default class Index extends Component {
     render() {
         return (
             <div>
+                <CustomHead />
+
                 <h1>Welcome to the 15 puzzle game!</h1>
             </div>
         );
