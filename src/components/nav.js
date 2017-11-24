0ae4eca5c3c205860ad6e44864dfe172c032bec3
Luwangel <adrien.amoros@gmail.com>
2017-11-24 13:38:57 +0100

    Create a nav bar component

---
+++ src/components/nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <nav className="blue lighten-1" role="navigation">
+                <div className="nav-wrapper container">
+                    <Link href="/">
+                        <a className="brand-logo">15 Puzzle Web!</a>
+                    </Link>
+                </div>
+            </nav>
+        );
+    }
+}
