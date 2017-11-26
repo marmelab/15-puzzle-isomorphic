9b505972bd9d234232b89b5b22642ab82943c47a
Luwangel <adrien.amoros@gmail.com>
2017-11-26 22:27:24 +0100

    Fix and connect the shuffler

--- pages/game.js
+++ pages/game.js
@@ -7,6 +7,7 @@ import Grid from '../src/components/grid';
 import ActivityIndicator from '../src/components/activityIndicator';
 
 import { buildGrid } from '../src/core/game';
+import { shuffle } from '../src/core/shuffler';
 
 export default class Index extends Component {
     state = {
@@ -27,10 +28,11 @@ export default class Index extends Component {
 
     componentWillMount = async () => {
         let resolvedGrid = buildGrid(4);
+        let currentGrid = await shuffle(resolvedGrid);
 
         this.setState({
             isLoading: false,
-            currentGrid: resolvedGrid,
+            currentGrid,
             resolvedGrid,
             turn: 0,
             isVictory: false,
