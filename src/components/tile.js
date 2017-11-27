a02a865e058c439f8b52590b2595e733d724d048
Luwangel <adrien.amoros@gmail.com>
2017-11-27 16:01:16 +0100

    Use classname in the tile component

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ClassNames from 'classnames';
 
 const Tile = ({ enabled, onClick, tileValue }) => {
     const handleClick = () => {
@@ -7,14 +8,14 @@ const Tile = ({ enabled, onClick, tileValue }) => {
             onClick(tileValue);
         }
     };
+    const tileClass = ClassNames({
+        'puzzle-tile': true,
+        'puzzle-tile-hover': enabled,
+        'z-depth-3': enabled,
+    });
 
     return (
-        <div
-            className={`puzzle-tile ${
-                enabled ? 'puzzle-tile-hover z-depth-3' : ''
-            }`}
-            onClick={handleClick}
-        >
+        <div className={tileClass} onClick={handleClick}>
             <span className="puzzle-tile-value">{tileValue}</span>
         </div>
     );
