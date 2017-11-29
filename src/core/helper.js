e8ad18d14731ac620939e2e3b6a073e473292b15
Luwangel <adrien.amoros@gmail.com>
2017-11-29 10:59:51 +0100

    Test the helper method and fix the empty grid error

--- src/core/helper.js
+++ src/core/helper.js
@@ -1,5 +1,6 @@
 export const associateTileToBackground = grid => {
-    const tileSize = 100 / grid.length; // 100%
+    const tileSize = grid.length > 0 ? 100 / grid.length : 0; // 100%
+
     let tileToBg = {};
     grid.forEach((row, rowKey) => {
         row.forEach((value, colKey) => {
