a2fe7cd32a194ccfa19b63f3081a4438a9e6a7b7
Luwangel <adrien.amoros@gmail.com>
2017-11-30 13:40:09 +0100

    Add a fetch error handler service

---
+++ __tests__/src/services/fetchErrorsHandler.spec.js
@@ -0,0 +1,26 @@
+import fetchErrorsHandler from '../../../src/services/fetchErrorsHandler';
+
+describe('HandleFetchErrors', () => {
+    test('should return a resolved promise containing the response if ok', async () => {
+        const response = {
+            ok: true,
+        };
+
+        expect.assertions(1);
+        return fetchErrorsHandler(response).then(result =>
+            expect(result).toEqual(response),
+        );
+    });
+
+    test('should return a rejected promise if not ok', async () => {
+        const response = {
+            ok: false,
+            statusText: 'Request failed',
+        };
+
+        expect.assertions(1);
+        return fetchErrorsHandler(response).catch(err => {
+            expect(err).toEqual('Request failed');
+        });
+    });
+});
