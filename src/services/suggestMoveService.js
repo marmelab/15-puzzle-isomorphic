b2388ef1f4123032d0129455fcdb2b3acd691b50
Luwangel <adrien.amoros@gmail.com>
2017-11-30 18:33:53 +0100

    Remove the json header in suggest service

--- src/services/suggestMoveService.js
+++ src/services/suggestMoveService.js
@@ -5,11 +5,6 @@ import { addGetParams } from './urlBuilder';
 import config from '../config';
 const DEFAULT_BASE_URL = config.apiUrl.suggest;
 
-const DEFAULT_HEADER_JSON = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-};
-
 export const suggest = (baseUrl = DEFAULT_BASE_URL) => (grid, resolvedGrid) => {
     const url = addGetParams(`${baseUrl}/suggest`, {
         grid: JSON.stringify(grid),
@@ -18,7 +13,6 @@ export const suggest = (baseUrl = DEFAULT_BASE_URL) => (grid, resolvedGrid) => {
 
     return fetch(url, {
         method: 'GET',
-        headers: Object.assign({}, DEFAULT_HEADER_JSON),
     })
         .then(fetchErrorsHandler)
         .then(res => res.json());
