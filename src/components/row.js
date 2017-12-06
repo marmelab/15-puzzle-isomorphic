796b31b3630b98c6b647e443ca4681dc66bede18
Luwangel <adrien.amoros@gmail.com>
2017-12-06 18:15:32 +0100

    Fix the row element

--- src/components/row.js
+++ src/components/row.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types';
 const Row = ({ children }) => <div className="row">{children}</div>;
 
 Row.propTypes = {
-    children: PropTypes.element.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.arrayOf(PropTypes.element),
+    ]).isRequired,
 };
 
 export default Row;
