b59d7a84db54b7340dbcabc379eee2428c6253fc
Luwangel <adrien.amoros@gmail.com>
2017-12-08 12:24:31 +0100

    Create a translate transition

--- src/components/transitions.js
+++ src/components/transitions.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { Transition } from 'react-transition-group';
 
 const DURATION = 375;
+const BASE_TRANSITION = `${DURATION}ms cubic-bezier(0.4, 0.0, 0.2, 1)`;
 
 const fadeDefaultStyle = {
     opacity: 0,
     visibility: 'hidden',
-    transition: `opacity ${DURATION}ms cubic-bezier(0.4, 0.0, 0.2, 1)`,
+    transition: `opacity ${BASE_TRANSITION}`,
 };
 
 const fadeTransitionStyles = {
@@ -38,6 +39,48 @@ export class Fade extends PureComponent {
                         },
                     })
                 }
+            </Transition>
+        );
+    }
+}
+
+const translateDefaultStyle = {
+    transform: 'translate(0, 0)',
+    transition: `transform ${BASE_TRANSITION}`,
+};
+
+const translateTransitionStyles = (x = 0, y = 0) => ({
+    entering: { transform: 'translate(0)' },
+    entered: { transform: `translate(${x}, ${y})` },
+});
+
+export class Translate extends PureComponent {
+    static propTypes = {
+        children: PropTypes.oneOfType([PropTypes.element, PropTypes.node])
+            .isRequired,
+        dir: PropTypes.object.isRequired,
+        timeout: PropTypes.number,
+    };
+
+    static defaultProps = {
+        timeout: DURATION,
+    };
+
+    render() {
+        const { children, dir, ...props } = this.props;
+
+        return (
+            <Transition {...props}>
+                {state => {
+                    return React.cloneElement(children, {
+                        ...children.props,
+                        style: {
+                            ...children.props.style,
+                            ...translateDefaultStyle,
+                            ...translateTransitionStyles(dir.x, dir.y)[state],
+                        },
+                    });
+                }}
             </Transition>
         );
     }
